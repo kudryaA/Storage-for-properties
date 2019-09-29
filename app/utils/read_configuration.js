3a14aa66eeef723d9a447b70fa953f48386c079e
Anton Kudryavtsev <kam123ua@gmail.com>
2019-09-29 16:35:14 +0300

    Add method for get configuration

---
+++ app/utils/read_configuration.js
@@ -0,0 +1,21 @@
+const { parse } = require('yaml');
+const { existsSync, readFileSync } = require('fs');
+
+exports.getConfiguration = (path) => {
+  const result = {
+    host: '0.0.0.0',
+    port: 4040,
+    security: false
+  };
+  if (existsSync(path)) {
+    const text = readFileSync(path, 'utf-8');
+    const yaml = parse(text);
+    result.host = yaml.host || result.host;
+    result.port = yaml.port || result.port;
+    if (yaml.security && yaml.password) {
+      result.security = true;
+      result.password = yaml.password;
+    }
+  }
+  return result;
+};
